e34ccbb70e50c6d87995ef14e154a76ff8d95a79
Michael Gobz <47690301+michaelgobz@users.noreply.github.com>
2020-04-08 19:49:26 +0300

    linting1

--- src/estimator.js
+++ src/estimator.js
@@ -1,24 +1,21 @@
-const covid19ImpactEstimator = (data) => {
-    return {
-        data: {},
-        Impact: {
-            currentlyInfected: () => {
-                return data.reportedCases * 10;
-            },
-            infectionsByRequestedTime: () => {
-                return this.currentlyInfected * 512;
-            }
+const covid19ImpactEstimator = (data) => ({
+    data: {},
+    Impact: {
+        currentlyInfected: () => {
+            return data.reportedCases * 10;
         },
-        severeImpact: {
-            currentlyInfected: () => {
-                return data.reportedCases * 50;
-            },
-            infectionsByRequestedTime: () => {
-                return  this.currentlyInfected * 512;
-            }
+        infectionsByRequestedTime: () => {
+            return this.currentlyInfected * 512;
         }
-
+    },
+    severeImpact: {
+        currentlyInfected: () => {
+            return data.reportedCases * 50;
+        },
+        infectionsByRequestedTime: () => {
+            return this.currentlyInfected * 512;
+        }
     }
-}
+})
 
 export default covid19ImpactEstimator;
