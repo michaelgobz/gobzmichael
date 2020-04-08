5b07c1d95b6517e6dc992b161507d2283199a580
Michael Gobz <47690301+michaelgobz@users.noreply.github.com>
2020-04-08 20:32:00 +0300

    done

--- src/estimator.js
+++ src/estimator.js
@@ -1,16 +1,12 @@
 const covid19ImpactEstimator = (data) => ({
   data: {},
   Impact: {
-    currentlyInfected: () => {
-      return data.reportedCases * 10;
-    },
-    infectionsByRequestedTime: () => { return Impact.currentlyInfected * 512; 
-    },
+    currentlyInfected: () => data.reportedCases * 10,
+    infectionsByRequestedTime: () => Impact.currentlyInfected * 512
+  },
   severeImpact: {
-    currentlyInfected: () => {
-      return data.reportedCases * 50;
-    },
-    infectionsByRequestedTime: () => { return severeImpact.currentlyInfected * 512;
+    currentlyInfected: () => data.reportedCases * 50,
+    infectionsByRequestedTime: () => severeImpact.currentlyInfected * 512
   }
 });
 export default covid19ImpactEstimator;
