e907bb4d8d75e7ae11d39d4f9c356d1278a51f24
Michael Gobz <47690301+michaelgobz@users.noreply.github.com>
2020-04-09 13:46:03 +0300

    data_5

--- src/estimator.js
+++ src/estimator.js
@@ -1,21 +1,21 @@
 // impact case requested by time
 function impactInfectionsByRequestedTime(data) {
-  if (data === 'days') {
+  if (data.periodType === 'days') {
     return (Math.trunc(data.timeToElapse / 3));
-  } if (data === 'weeks') {
+  } if (data.periodType === 'weeks') {
     return (Math.trunc(((data.timeToElapse * 7) / 3)));
-  } if (data === 'months') {
+  } if (data.periodType === 'months') {
     return (Math.trunc(((data.timeToElapse * 30) / 3)));
   }
   return 0;
 }
 // severeImpact cases requeted by time
 function severeInfectionsByRequestTime(data) {
-  if (data === 'days') {
+  if (data.periodType === 'days') {
     return (Math.trunc(data.timeToElapse / 3));
-  } if (data === 'weeks') {
+  } if (data.periodType === 'weeks') {
     return (Math.trunc(((data.timeToElapse * 7) / 3)));
-  } if (data === 'months') {
+  } if (data.periodType === 'months') {
     return (Math.trunc(((data.timeToElapse * 30) / 3)));
   }
   return 0;
